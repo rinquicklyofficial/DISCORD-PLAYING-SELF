b0311329274fec3f1e81cac4e95a84e24e609cbc
rinquickly <content@rinquickly.qzz.io>
2026-01-03 09:39:35 +0700

    Update and rename index (1).js to index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client } = require('discord.js-selfbot-v13');
 
 const client = new Client();
 
-const TOKEN = "MTQ1Mzk0NzIwNjAyNDQyOTcwNA.GzgEVV.M05DQK1-Da3kfY9RJ1aVxJ1F-TEvZbQcVSh6vs"; // <-- Put your user token here
+const TOKEN = "YOU SELF TOKEN"; // <-- Put your user token here
 
 client.on('ready', () => {
   console.log(`✅ Online as ${client.user.tag}`);
